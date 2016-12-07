8ff56286a75f5a45074ad55e035a9d8b09f18c1e
Moodah <christopher.browne@heig-vd.ch>
2016-12-07 13:41:21 +0100

    test heroku config

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@
 
 const app = require('./app');
 const port = app.get('port');
-const server = app.listen(port);
+const server = app.listen(process.env.port);
 
 server.on('listening', () =>
-  console.log(`Feathers application started on ${app.get('host')}:${port}`)
+  console.log(`Feathers application started on ${app.get('host')}:${process.env.port}`)
 );
